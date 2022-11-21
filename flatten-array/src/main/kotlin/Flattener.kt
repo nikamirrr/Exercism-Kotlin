585f337c98c5a3de7cd5b7948a0ae0e315e1b22a
Nikolay Mirin <nmirin@legalzoom.com>
2022-11-21 05:03:53 -0700

    flat

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,26 @@
+import java.util.*
+
 object Flattener {
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+        val myStack = Stack<Iterator<Any?>>()
+        myStack.push(source.iterator())
+        val result = ArrayList<Any>()
+
+        while (!myStack.empty()) {
+            val curIt = myStack.pop()
+            while (curIt.hasNext()) {
+                var curVal = curIt.next()
+
+                if (curVal is Collection<Any?>) {
+                    myStack.push(curIt)
+                    myStack.push(curVal.iterator())
+                    break
+                } else if (curVal != null) {
+                    result.add(curVal)
+                }
+
+            }
+        }
+        return result
     }
 }
