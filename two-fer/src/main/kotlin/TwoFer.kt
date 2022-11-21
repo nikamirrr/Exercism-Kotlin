49a790dcc76f132dd02d9a81324a726b286df805
Nikolay Mirin <nmirin@legalzoom.com>
2022-11-20 23:47:43 -0700

    twofer

--- two-fer/src/main/kotlin/TwoFer.kt
+++ two-fer/src/main/kotlin/TwoFer.kt
@@ -1,5 +1,3 @@
 fun twofer(name: String? = null): String {
-    if (name == null)
-        return "One for you, one for me."
-    return "One for $name, one for me."
+    return "One for " + (name?:"you") + ", one for me."
 }
