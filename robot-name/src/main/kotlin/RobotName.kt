03c1b676b26d0aaafeb0d5e2267537169ad8607b
Nikolay Mirin <nmirin@legalzoom.com>
2022-11-21 11:48:58 -0700

    robot

---
+++ robot-name/src/main/kotlin/RobotName.kt
@@ -0,0 +1,29 @@
+import javax.management.Query.div
+import kotlin.random.Random
+
+class Robot() {
+
+    private var storedName = generateName()
+    private companion object {
+        val takenNames = HashSet<Int>()
+    }
+
+
+    val name: String
+        get() = storedName
+
+    fun reset() {
+        storedName = generateName()
+    }
+
+    private fun generateName(): String {
+        var digitName = 0
+        do {
+            digitName = Random.nextInt(676000)
+        } while (takenNames.contains(digitName))
+        takenNames.add(digitName)
+        return "%c%c%03d".format(Char(65 + digitName % 26),
+            Char(65 + (digitName / 26) % 26),
+            digitName / 676)
+    }
+}
