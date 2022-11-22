91e8b56d1b066f82d7f9959490ea923fc7a3c8d8
Nikolay Mirin <nmirin@legalzoom.com>
2022-11-21 19:52:30 -0700

    raindrops

--- raindrops/src/main/kotlin/Raindrops.kt
+++ raindrops/src/main/kotlin/Raindrops.kt
@@ -3,9 +3,10 @@ import java.io.File.separator
 object Raindrops {
 
     fun convert(n: Int): String {
-        val temp = ((3..7 step 2).zip(listOf("Pling", "Plang", "Plong")).filter{ n % it.first == 0 })
-        if (temp.isEmpty())
-            return n.toString()
-        return temp.joinToString(separator=""){it -> it.second }
+        return ((3..7 step 2)
+            .zip(listOf("Pling","Plang", "Plong"))
+            .filter{ n % it.first == 0 })
+            .joinToString(separator=""){it -> it.second }
+            .takeIf{ it.isNotEmpty() }?:n.toString()
     }
 }
